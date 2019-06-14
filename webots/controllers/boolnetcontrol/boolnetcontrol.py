9c573b921d2719618a9af86cab423d789ddc8249
XanderC94 <alessandro.cevoli2@studio.unibo.it>
2019-06-14 09:50:57 +0200

    Add webots simulator controller files(.py)

---
+++ webots/controllers/boolnetcontrol/boolnetcontrol.py
@@ -0,0 +1,45 @@
+"""daca controller."""
+
+from bncontroller.sim.robot.agent import EPuck
+from bncontroller.sim.robot.core import BNController
+from bncontroller.sim.config import parse_args_to_config
+from bncontroller.sim.robot.utils import DeviceName
+from bncontroller.sim.robot.binarization import light_sensor_binarization
+from bncontroller.sim.logging.logger import FileLogger
+from bncontroller.sim.logging.datadumper import SimulationDataDumper
+
+config = parse_args_to_config()
+
+logger = FileLogger('BoolNetControl', path = config.sim_log_path)
+
+logger.suppress(config.sim_suppress_logging)
+
+#-------------------------------------------
+
+controller = BNController(config, binarization_strategies={
+    DeviceName.LIGHT : light_sensor_binarization,
+})
+
+epuck = EPuck(config)
+
+epuck.position = config.sim_agent_position
+epuck.orientation = config.sim_agent_y_rot
+
+dumper = SimulationDataDumper(config.sim_run_time_s, epuck.timestep)
+
+
+epuck.run(
+    controller, 
+    lambda string: logger.info(string), 
+    lambda data: dumper.add_log_entry(data)
+)
+
+dumper.dump_data(config.sim_data_path)
+
+# Cleanup code.
+
+dumper.clear()
+logger.flush()
+epuck.cleanup()
+
+# --------------------------------------------
---
+++ webots/controllers/demiurge/demiurge.py
@@ -0,0 +1,62 @@
+"""demiurge controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, LED, DistanceSensor
+from controller import Supervisor
+# import sys, os, copy
+# print(os.getcwd())
+
+from bncontroller.sim.config import parse_args_to_config
+from bncontroller.sim.robot.binarization import light_sensor_binarization
+from bncontroller.sim.logging.logger import FileLogger
+from bncontroller.sim.logging.datadumper import SimulationDataDumper
+
+config = parse_args_to_config()
+
+#-------------------------------------------
+
+demiurge = Supervisor()
+
+timestep = int(demiurge.getBasicTimeStep())
+
+try:
+    ls = demiurge.getFromDef(config.webots_nodes_defs['PointLight'])
+    ls.getField('location').setSFVec3f(list(config.sim_light_position))
+    # print(ls.getField('location').getSFVec3f())
+
+    # hyperuranium = demiurge.getFromDef(config.webots_nodes_defs['WorldInfo'])
+    # hyperuranium.getField('basicTimeStep').setSFFloat(config.sim_timestep_ms)
+    # print(hyperuranium.getField('basicTimeStep').getSFFloat())
+except Exception as ex:
+    print(ex)
+
+n_steps = 0
+max_steps = int((config.sim_run_time_s * 1000) / timestep)
+trigger_step = int((config.sim_event_timer_s * 1000) / timestep) # step @ which the event should be triggered
+
+# -------------------------------------------------
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while demiurge.step(timestep) != -1 and n_steps != max_steps:
+    
+    # -------------------- PERFORM SIMULATION STEP ------------------------
+    
+    if n_steps == trigger_step:
+        # trigger event modifying the simulation world
+        try:
+            ls = demiurge.getFromDef('LightSource')
+            ls.getField('intensity').setSFFloat(0.0)
+            print('Event Triggered')
+        except Exception as ex:
+            print(ex)
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~ LOGGING STUFF ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            
+    n_steps += 1
+
+    pass
+
+# Cleanup code.
+
+# --------------------------------------------
